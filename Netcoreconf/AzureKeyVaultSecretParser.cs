a65fc741e2c68a50e91897490fc12b7be91a1e03
Jorge Turrado <jorge_turrado@hotmail.es>
2022-09-02 22:27:38 +0200

    Add secret parser and keyvault scenario
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

---
+++ Netcoreconf/AzureKeyVaultSecretParser.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Security.KeyVault.Secrets;
+
+namespace Netcoreconf
+{
+    public class AzureKeyVaultSecretParser : KeyVaultSecretManager
+    {
+        public override string GetKey(KeyVaultSecret secret)
+            => secret.Name.Replace("--", ConfigurationPath.KeyDelimiter);
+    }
+}
--- Netcoreconf/Program.cs
+++ Netcoreconf/Program.cs
@@ -1,9 +1,20 @@
+using Azure.Identity;
+using Netcoreconf;
 using Netcoreconf.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Mount file provided by CSI Driver
 builder.Configuration.AddKeyPerFile("/mnt/secrets-store", optional: true);
+
+// Register Azure Key Vault as a configuration source
+if (!string.IsNullOrEmpty(builder.Configuration["KeyVaultName"]))
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
+        new DefaultAzureCredential(),
+        new AzureKeyVaultSecretParser());
+}
 
 // Register ServiceBus client 
 builder.Services.AddServiceBus(builder.Configuration);
