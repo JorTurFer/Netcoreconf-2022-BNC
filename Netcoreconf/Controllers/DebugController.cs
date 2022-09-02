92341b960d900b6962c278bf1c3f4f78364d05c6
Jorge Turrado <jorge_turrado@hotmail.es>
2022-09-02 23:35:19 +0200

    expose tokens
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

---
+++ Netcoreconf/Controllers/DebugController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Netcoreconf.Controllers
+{
+    public class DebugController: ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private const string serviceAccountTokenFile = "/var/run/secrets/kubernetes.io/serviceaccount/token";
+        private const string azureTokenFile = "/var/run/secrets/azure/tokens/azure-identity-token";
+
+
+        public DebugController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("environment")]
+        public IActionResult GetEnvirnmentVariables()
+        {
+            var envirnmentVariables = Environment.GetEnvironmentVariables();
+            return Ok(envirnmentVariables);
+        }
+
+        [HttpGet("configuration")]
+        public IActionResult GetConfiguration()
+        {
+            return Ok(_configuration.AsEnumerable());
+        }
+
+        [HttpGet("service-account-token")]
+        public IActionResult GetServiceAccountToke()
+        {
+            var token = "";
+            if (System.IO.File.Exists(serviceAccountTokenFile))
+            {
+                token = System.IO.File.ReadAllText(serviceAccountTokenFile);
+            }
+            return Ok(token);
+        }
+
+        [HttpGet("azure-tokent")]
+        public IActionResult GetAzureToken()
+        {
+            var token = "";
+            if (System.IO.File.Exists(azureTokenFile))
+            {
+                token = System.IO.File.ReadAllText(azureTokenFile);
+            }
+            return Ok(token);
+        }
+    }
+}
